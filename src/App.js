076ed5fd5c2d641493328f151405928d35df836c
Adam Faraj <adamf@marketamerica.com>
2020-05-28 09:28:35 -0400

    add forgot password and login stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
-import Login from './components/Login/Login'
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
 
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 import './App.css';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+
+// AWS amplify stuff
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: awsConfig.REGION,
+    userPoolId: awsConfig.USER_POOL_ID,
+    userPoolWebClientId: awsConfig.APP_CLIENT_ID
+  }
+});
+
 
 function App() {
-  return (
-    <Login />
+  // React Hooks
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  function setAuthStatus(authenticated) {
+    // this.setState({ isAuthenticated: authenticated });
+    setIsAuthenticated(authenticated);
+  }
+
+  function setAWSUser(user) {
+    // this.setState({ user: user });
+    setUser(user);
+    console.log('app state', this.state);
+  }
+
+  const authProps = {
+    isAuthenticated: isAuthenticated,
+    user: user,
+    setAuthStatus: setAuthStatus,
+    setUser: setAWSUser
+  }
+
+  if(isAuthenticated) {
+    return <Home />;
+  }
+  return(
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login screenProps={authProps}/>
+        </Route>
+        <Route path="/forgot-password">
+          <ForgotPassword />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,80 @@
-import React from 'react';
-import './Login.css';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory
+} from "react-router-dom";
 
-export default function Login() {
+import { Auth } from 'aws-amplify';
+import {TextField, Button, Typography} from '@material-ui/core';
+
+import ForgotPassword from '../ForgotPassword/ForgotPassword';
+import './Login.css';
+import Home from '../Home/Home';
+
+export default function Login({screenProps}) {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState('');
+  // const [user, setUser] = useState(null);
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(`${email}`);
+  // }
+
+  const handleSubmit = async event => {
+    console.log(screenProps)
+    event.preventDefault();
+    try {
+      const user = await Auth.signIn(email, password)
+        // .then(
+        //   render() { }
+        // );
+      console.log(user)
+      screenProps.setUser(user);
+      screenProps.setAuthStatus(true);
+      // console.log('user ', user);
+      // console.log('state ', this.state);
+      // render() {
+        // return <AppNavigator />;
+      // }
+      // this.props.navigation.navigate('AppNav');
+    } catch (error) {
+      let err = null;
+      !error.message ? err = { "message": error } : err = error;
+      setErrors({
+        errors: {
+          ...errors,
+          cognito: err
+        }
+      });
+      console.error(error);
+    }
+  };
+
+  function onForgotPassword() {
+    let path = `newPath`;
+    // history.push('/forgot-password')
+    history.push('/forgot-password')
+    console.log(history);
+  }
+
   return(
-    <div class="container">
-      <h2>Kuruma Works</h2>
-      <form>
-          <input type="email" placeholder="Email" required/>
-          <input type="password" placeholder="Password" required/>
-      </form>
-    </div>
+      <div className="container">
+        <h1>Kuruma Works</h1>
+        <form onSubmit={handleSubmit}>
+            {/* <input type="email" placeholder="Email" required/> */}
+            <TextField label="Email" required onChange={e => setEmail(e.target.value)}/>
+            <TextField label="Password" type="password" required onChange={e => setPassword(e.target.value)}/>
+            {/* <input type="password" placeholder="Password" required/> */}
+            {/* <Button color="primary" onClick={onForgotPassword}>Forgot Your Password?</Button> */}
+            <Link to="/forgot-password">Forgot Your Password?</Link>
+            <Button type="submit" className="login-btn" variant="contained">Login</Button>
+        </form>
+        <Button className="register-btn" color="primary" variant="contained">Register</Button>
+      </div>
   );
 }
